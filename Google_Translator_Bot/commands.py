8b224da4a25424b59740c1cf4c772d833a6883e1
PR0FESS0R-99 <74817426+PR0FESS0R-99@users.noreply.github.com>
2021-09-03 23:13:47 +0530

    Update commands.py

--- Google_Translator_Bot/commands.py
+++ Google_Translator_Bot/commands.py
@@ -4,3 +4,14 @@ from pyrogram import Client, filters
 @Client.on_message(filters.private & filters.command("start"))
 async def start_main(main, update):
     await update.reply_text("Hy")
+
+
+@Client.on_message(filters.private & filters.text)
+async def translator(client, msg):
+  
+msg.reply_text(
+"Select Your language 👇",
+reply_to_message_id = msg.message_id,
+reply_markup = LANGUAGE
+)
+
