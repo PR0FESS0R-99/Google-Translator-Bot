d8bba23758086e7f1cdd0cd0968ebf9e93730951
PR0FESS0R-99 <74817426+PR0FESS0R-99@users.noreply.github.com>
2021-09-03 23:21:58 +0530

    Create translator.py

---
+++ Google_Translator_Bot/translator.py
@@ -0,0 +1,20 @@
+from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery 
+from Google_Translator_Bot.Language import LANGUAGE
+
+@Client.on_message(filters.private & filters.text)
+async def translator(client, msg):
+  
+msg.reply_text(
+"Select Your language 👇",
+reply_to_message_id = msg.message_id,
+reply_markup = LANGUAGE
+)
+
+@Client.on_callback_query()
+async def translate_text(bot,update):
+  tr_text = update.message.reply_to_message.text
+  cbdata = update.data
+  translator = google_translator()
+  translated_text = translator.translate(tr_text,lang_tgt=cbdata)
+  await update.message.edit(translated_text)
