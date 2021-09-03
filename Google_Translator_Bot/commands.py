90ce3b82bad3ffb7467389b5e93dd96b078206e7
PR0FESS0R-99 <74817426+PR0FESS0R-99@users.noreply.github.com>
2021-09-03 23:19:26 +0530

    Update commands.py

--- Google_Translator_Bot/commands.py
+++ Google_Translator_Bot/commands.py
@@ -6,12 +6,5 @@ async def start_main(main, update):
     await update.reply_text("Hy")
 
 
-@Client.on_message(filters.private & filters.text)
-async def translator(client, msg):
-  
-msg.reply_text(
-"Select Your language 👇",
-reply_to_message_id = msg.message_id,
-reply_markup = LANGUAGE
-)
 
+  	
