a3152889525c819fbf264d9897dc9f0622cb96f0
PR0FESS0R-99 <74817426+PR0FESS0R-99@users.noreply.github.com>
2021-09-03 23:42:32 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,12 @@
+import os
+
+class Config(object):
+     
+    BO_TOKEN = os.environ.get("BOT_TOKEN", "")
+
+
+    API_ID = int(os.environ.get("API_ID", 12345))
+
+
+    API_HASH = os.environ.get("API_HASH", "")
 
