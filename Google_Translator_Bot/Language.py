def2524ee251965ede120465a3097cb4e1897196
PR0FESS0R-99 <74817426+PR0FESS0R-99@users.noreply.github.com>
2021-09-04 15:24:45 +0530

    Update Language.py

--- Google_Translator_Bot/Language.py
+++ Google_Translator_Bot/Language.py
@@ -1,7 +1,77 @@
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery 
 
 LANGUAGE = InlineKeyboardMarkup([[
-InlineKeyboardButton("English", callback_data='en'),
-InlineKeyboardButton("മലയാളം", callback_data='ml')
-]]
-)
+
+ InlineKeyboardButton("Afrikaans", callback_data='af')
+ ],[
+ InlineKeyboardButton("Albanian", callback_data='sq')
+ ],[
+ InlineKeyboardButton("Amharic",callback_data ='am')
+ ],[  
+ InlineKeyboardButton("Arabic", callback_data='ar')
+ ],[
+ InlineKeyboardButton("Armenian", callback_data='hy') 
+ ],[
+ InlineKeyboardButton("Azerbaijani",callback_data = 'az')       
+ ],[
+ InlineKeyboardButton("Basque",callback_data ="eu")
+ ],[
+ InlineKeyboardButton("Belarusian",callback_data ="be")
+ ],[
+ InlineKeyboardButton("Bengali",callback_data="bn")
+ ],[
+ InlineKeyboardButton("Bosnian",callback_data = "bs")
+ ],[
+ InlineKeyboardButton("Bulgarian",callback_data ="bg")
+ ],[
+ InlineKeyboardButton("Catalan",callback_data = "ca")
+ ],[ 
+ InlineKeyboardButton("Corsican",callback_data ="co")
+ ],[
+ InlineKeyboardButton("Croatian",callback_data = "hr")
+ ],[
+ InlineKeyboardButton("Czech", callback_data = "cs")
+ ],
+ InlineKeyboardButton("Danish",callback_data = "da")
+ ],[
+ InlineKeyboardButton("Dutch",callback_data = "nl")
+ ],[
+ InlineKeyboardButton("Esperanto",callback_data = "eo")
+
+ InlineKeyboardButton("English",callback_data = "en")
+ ],[
+ InlineKeyboardButton("Estonian",callback_data = "et")
+ ],[
+ InlineKeyboardButton("Finnish",callback_data = "fi")
+ ],[
+ InlineKeyboardButton("French",callback_data = "fr")
+ ],[
+ InlineKeyboardButton("Frisian",callback_data = "fy")
+ ],[
+ InlineKeyboardButton("Galician",callback_data = "gl")
+ ],[
+ InlineKeyboardButton("Georgian",callback_data = "ka")
+ ],[
+ InlineKeyboardButton("German",callback_data = "de")
+ ],[
+ InlineKeyboardButton("Greek",callback_data = "el")
+ ],[
+ InlineKeyboardButton("Gujarati",callback_data = "gu")
+ ],[
+ InlineKeyboardButton("Haitian Creole",callback_data = "ht")
+ ],[
+ InlineKeyboardButton("Hausa",callback_data ="ha")
+ ],[
+ InlineKeyboardButton("Hindi",callback_data = "hi")
+ ],[
+ InlineKeyboardButton("Hungarian",callback_data = "hu")
+ ],[
+ InlineKeyboardButton("Icelandic",callback_data = "is")
+ ],[
+ InlineKeyboardButton("Igbo",callback_data = "ig")
+ ],[
+ InlineKeyboardButton("Indonesian",callback_data = "id")
+ ],[
+ InlineKeyboardButton("Irish",callback_data = "ga")
+ ]]
+ )
