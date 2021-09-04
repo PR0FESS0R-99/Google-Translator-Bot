8d6122d750d9ae0659ea643aff41dabbfdff9632
PR0FESS0R-99 <74817426+PR0FESS0R-99@users.noreply.github.com>
2021-09-04 13:21:02 +0530

    Update commands.py

--- Google_Translator_Bot/commands.py
+++ Google_Translator_Bot/commands.py
@@ -4,11 +4,11 @@ from translation import Translation
 
 @Client.on_message(filters.private & filters.command("start"))
 async def start_main(main, update):
-    await update.reply_text(Translation.START_MSG,
+    await update.reply_text(Translation.START_MSG.format(update.from_user.mention),
                             reply_to_message_id = update.message_id,
                             parse_mode="markdown",
                             reply_markup=InlineKeyboardMarkup([[
-                            InlineKeyboardButton("📢Bot Updates" ,url="t.me/Mo_Tech_YT")
+                            InlineKeyboardButton("📢 Join Updates Channel 📢" ,url="t.me/Mo_Tech_YT")
                             ]])
                             ) 
                   
