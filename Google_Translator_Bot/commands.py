a70c4c7793f5a396667b72d741e93d08b4b4c124
PR0FESS0R-99 <74817426+PR0FESS0R-99@users.noreply.github.com>
2021-09-04 13:13:06 +0530

    Update commands.py

--- Google_Translator_Bot/commands.py
+++ Google_Translator_Bot/commands.py
@@ -1,9 +1,16 @@
 from pyrogram import Client, filters
-
+from translation import Translation
 
 @Client.on_message(filters.private & filters.command("start"))
 async def start_main(main, update):
-    await update.reply_text("Hy")
+    await update.reply_text(Translation.START_MSG,
+                            reply_to_message_id = message.message_id,
+                            parse_mode="markdown",
+                            reply_markup=InlineKeyboardMarkup([[
+                            InlineKeyboardButton("📢Bot Updates" ,url="t.me/Mo_Tech_YT")
+                            ]])
+                            ) 
+                  
 
 
 
