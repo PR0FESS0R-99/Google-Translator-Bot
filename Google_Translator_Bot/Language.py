e204c6288342096daf167b3e291e20f8272b5fb9
PR0FESS0R-99 <74817426+PR0FESS0R-99@users.noreply.github.com>
2021-09-04 13:16:48 +0530

    Update Language.py

--- Google_Translator_Bot/Language.py
+++ Google_Translator_Bot/Language.py
@@ -1,6 +1,7 @@
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery 
 
-LANGUAGE = InlineKeyboardMarkup( [[
-InlineKeyboardButton("English", callback_data='en')
+LANGUAGE = InlineKeyboardMarkup([[
+InlineKeyboardButton("English", callback_data='en'),
+InlineKeyboardButton("മലയാളം", callback_data='ml')
 ]]
 )
