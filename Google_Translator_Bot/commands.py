d207343d5b8dc7ba6cf7d55f14cf2da9081768b6
PR0FESS0R-99 <74817426+PR0FESS0R-99@users.noreply.github.com>
2021-09-04 13:17:33 +0530

    Update commands.py

--- Google_Translator_Bot/commands.py
+++ Google_Translator_Bot/commands.py
@@ -1,4 +1,5 @@
 from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from translation import Translation
 
 @Client.on_message(filters.private & filters.command("start"))
