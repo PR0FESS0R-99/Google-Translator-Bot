80e39e745ae9931a9a4a9994a06a5bb5699e740b
PR0FESS0R-99 <74817426+PR0FESS0R-99@users.noreply.github.com>
2021-09-04 14:50:36 +0530

    Update translation.py

--- translation.py
+++ translation.py
@@ -1,6 +1,6 @@
 class Translation(object):
 
-    START_MSG = """👋Hello **{}**
+    START_MSG = """👋Hello {}
 
 __I am simple Google Translater Bot \n I can translate any language to you selected language__
 """
