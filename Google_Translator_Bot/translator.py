f94cfae6db6fe7c5c65560e060fe36a00941630a
PR0FESS0R-99 <74817426+PR0FESS0R-99@users.noreply.github.com>
2021-09-04 11:47:05 +0530

    Update translator.py

--- Google_Translator_Bot/translator.py
+++ Google_Translator_Bot/translator.py
@@ -5,10 +5,17 @@ from googletrans import Translator
 
 @Client.on_message(filters.private & filters.text)
 async def translator(client, message):
+    await message.reply_text(
+                             "Select language 👇",
+                             reply_to_message_id = message.message_id,
+                             reply_markup = LANGUAGE ) 
+
+
+@Client.on_callback_query()
+async def translate_text(bot,update):
   translator_text = update.message.reply_to_message.text
   cb_data = update.data
-  if cb_data== "page":
-  	await update.message.edit("Select language 👇",reply_markup = LANGUAGE)   
+
   else :
        translator = Translator()
        translated_msg = translator.translate(translator_text,lang_tgt=cbdata)
