2ee165c23c1060a48f5ae9a7f8f371a0f2838d3c
PR0FESS0R-99 <74817426+PR0FESS0R-99@users.noreply.github.com>
2021-09-04 19:26:09 +0530

    Update translator.py

--- Google_Translator_Bot/translator.py
+++ Google_Translator_Bot/translator.py
@@ -5,15 +5,11 @@ from Google_Translator_Bot.Language import LANGUAGE
 from translation import Translation
 
 @Client.on_message(filters.text & filters.private )
-async def echo(client, message):
-
- 
+async def echo(client, message): 
  await  message.reply_text(Translation.TRANSLATED_MSG,
                            reply_to_message_id = message.message_id,
                            reply_markup = LANGUAGE
-                           ) 
-
-    
+                           )     
 @Client.on_callback_query()
 async def translate_text(bot,update):
   translation_text = update.message.reply_to_message.text
