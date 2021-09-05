75aa3b9721f6263d6e3b296233b70c718da7a2f6
PR0FESS0R-99 <74817426+PR0FESS0R-99@users.noreply.github.com>
2021-09-05 12:19:20 +0530

    Update translation.py

--- translation.py
+++ translation.py
@@ -2,7 +2,7 @@ class Translation(object):
 
     START_MSG = """👋Hello {}\n\n I am simple Google Translater Bot \n I can translate any language to your desired language"""
 
-    TRANSLATED_MSG = """Select Your Language Shown below to translate.👇"""
+    TRANSLATED_MSG = """Choose The language From here That I Want to Translate.👇"""
 
     ABOUT_MSG = """🤖 Bot : Google Translator\n\n👨‍🎓 Credit : @PR0FESS0R_99\n\n🎙️ Language : Python3\n\n📚 Lybrary : Pyrogram v1.2.9\n\n🛑 Server : Heroku\n\n📱 Build :V0.3"""
 
