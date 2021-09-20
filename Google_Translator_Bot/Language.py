021ade6d1e8b67cedc7f74871b4c421d4f1759c3
Shahsad Kolathur <shahsadkpklr@gmail.com>
2021-09-20 14:15:34 +0530

    Added Back to Language List Button and Some Improvements

--- Google_Translator_Bot/Language.py
+++ Google_Translator_Bot/Language.py
@@ -2,208 +2,208 @@ from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, CallbackQ
 
 LANGUAGE = InlineKeyboardMarkup(
  [[
- InlineKeyboardButton("Afrikaans", callback_data='af')
+ InlineKeyboardButton("Afrikaans", callback_data='lang af')
  ],[
- InlineKeyboardButton("Albanian", callback_data='sq')
+ InlineKeyboardButton("Albanian", callback_data='lang sq')
  ],[
- InlineKeyboardButton("Amharic",callback_data ='am')
+ InlineKeyboardButton("Amharic",callback_data ='lang am')
  ],[  
- InlineKeyboardButton("Arabic", callback_data='ar')
+ InlineKeyboardButton("Arabic", callback_data='lang ar')
  ],[
- InlineKeyboardButton("Armenian", callback_data='hy') 
+ InlineKeyboardButton("Armenian", callback_data='lang hy') 
  ],[
- InlineKeyboardButton("Azerbaijani",callback_data = 'az')       
+ InlineKeyboardButton("Azerbaijani",callback_data = 'lang az')       
  ],[
- InlineKeyboardButton("Basque",callback_data ="eu")
+ InlineKeyboardButton("Basque",callback_data ="lang eu")
  ],[
- InlineKeyboardButton("Belarusian",callback_data ="be")
+ InlineKeyboardButton("Belarusian",callback_data ="lang be")
  ],[
- InlineKeyboardButton("Bengali",callback_data="bn")
+ InlineKeyboardButton("Bengali",callback_data="lang bn")
  ],[
- InlineKeyboardButton("Bosnian",callback_data = "bs")
+ InlineKeyboardButton("Bosnian",callback_data = "lang bs")
  ],[
- InlineKeyboardButton("Bulgarian",callback_data ="bg")
+ InlineKeyboardButton("Bulgarian",callback_data ="lang bg")
  ],[
- InlineKeyboardButton("Catalan",callback_data = "ca")
+ InlineKeyboardButton("Catalan",callback_data = "lang ca")
  ],[ 
- InlineKeyboardButton("Corsican",callback_data ="co")
+ InlineKeyboardButton("Corsican",callback_data ="lang co")
  ],[
- InlineKeyboardButton("Croatian",callback_data = "hr")
+ InlineKeyboardButton("Croatian",callback_data = "lang hr")
  ],[
- InlineKeyboardButton("Czech", callback_data = "cs")
+ InlineKeyboardButton("Czech", callback_data = "lang cs")
  ],[
- InlineKeyboardButton("Danish",callback_data = "da")
+ InlineKeyboardButton("Danish",callback_data = "lang da")
  ],[
- InlineKeyboardButton("Dutch",callback_data = "nl")
+ InlineKeyboardButton("Dutch",callback_data = "lang nl")
  ],[
- InlineKeyboardButton("Esperanto",callback_data = "eo")
+ InlineKeyboardButton("Esperanto",callback_data = "lang eo")
  ],[
- InlineKeyboardButton("English",callback_data = "en")
+ InlineKeyboardButton("English",callback_data = "lang en")
  ],[
- InlineKeyboardButton("Estonian",callback_data = "et")
+ InlineKeyboardButton("Estonian",callback_data = "lang et")
  ],[
- InlineKeyboardButton("Finnish",callback_data = "fi")
+ InlineKeyboardButton("Finnish",callback_data = "lang fi")
  ],[
- InlineKeyboardButton("French",callback_data = "fr")
+ InlineKeyboardButton("French",callback_data = "lang fr")
  ],[
- InlineKeyboardButton("Frisian",callback_data = "fy")
+ InlineKeyboardButton("Frisian",callback_data = "lang fy")
  ],[
- InlineKeyboardButton("Galician",callback_data = "gl")
+ InlineKeyboardButton("Galician",callback_data = "lang gl")
  ],[
- InlineKeyboardButton("Georgian",callback_data = "ka")
+ InlineKeyboardButton("Georgian",callback_data = "lang ka")
  ],[
- InlineKeyboardButton("German",callback_data = "de")
+ InlineKeyboardButton("German",callback_data = "lang de")
  ],[
- InlineKeyboardButton("Greek",callback_data = "el")
+ InlineKeyboardButton("Greek",callback_data = "lang el")
  ],[
- InlineKeyboardButton("Gujarati",callback_data = "gu")
+ InlineKeyboardButton("Gujarati",callback_data = "lang gu")
  ],[
- InlineKeyboardButton("Haitian Creole",callback_data = "ht")
+ InlineKeyboardButton("Haitian Creole",callback_data = "lang ht")
  ],[
- InlineKeyboardButton("Hausa",callback_data ="ha")
+ InlineKeyboardButton("Hausa",callback_data ="lang ha")
  ],[
- InlineKeyboardButton("Hindi",callback_data = "hi")
+ InlineKeyboardButton("Hindi",callback_data = "lang hi")
  ],[
- InlineKeyboardButton("Hungarian",callback_data = "hu")
+ InlineKeyboardButton("Hungarian",callback_data = "lang hu")
  ],[
- InlineKeyboardButton("Icelandic",callback_data = "is")
+ InlineKeyboardButton("Icelandic",callback_data = "lang is")
  ],[
- InlineKeyboardButton("Igbo",callback_data = "ig")
+ InlineKeyboardButton("Igbo",callback_data = "lang ig")
  ],[
- InlineKeyboardButton("Indonesian",callback_data = "id")
+ InlineKeyboardButton("Indonesian",callback_data = "lang id")
  ],[
- InlineKeyboardButton("Irish",callback_data = "ga")
+ InlineKeyboardButton("Irish",callback_data = "lang ga")
  ],[
- InlineKeyboardButton("Italian",callback_data = "it")
+ InlineKeyboardButton("Italian",callback_data = "lang it")
  ],[
- InlineKeyboardButton("Japanese",callback_data = "ja")
+ InlineKeyboardButton("Japanese",callback_data = "lang ja")
  ],[
- InlineKeyboardButton("Javanese",callback_data = "jv")
+ InlineKeyboardButton("Javanese",callback_data = "lang jv")
  ],[
- InlineKeyboardButton("Kannada",callback_data = "kn")
+ InlineKeyboardButton("Kannada",callback_data = "lang kn")
  ],[
- InlineKeyboardButton("Kazakh",callback_data = "kk")
+ InlineKeyboardButton("Kazakh",callback_data = "lang kk")
  ],[
- InlineKeyboardButton("Khmer",callback_data = "km")
+ InlineKeyboardButton("Khmer",callback_data = "lang km")
  ],[
- InlineKeyboardButton("Kinyarwanda",callback_data = "rw")
+ InlineKeyboardButton("Kinyarwanda",callback_data = "lang rw")
  ],[
- InlineKeyboardButton("Korean",callback_data ="ko")
+ InlineKeyboardButton("Korean",callback_data ="lang ko")
  ],[
- InlineKeyboardButton("Kurdish",callback_data = "ku")
+ InlineKeyboardButton("Kurdish",callback_data = "lang ku")
  ],[
- InlineKeyboardButton("Kyrgyz",callback_data ="ky")
+ InlineKeyboardButton("Kyrgyz",callback_data ="lang ky")
  ],[
- InlineKeyboardButton("Lao",callback_data = "lo")
+ InlineKeyboardButton("Lao",callback_data = "lang lo")
  ],[
- InlineKeyboardButton("Latin",callback_data = "la")
+ InlineKeyboardButton("Latin",callback_data = "lang la")
  ],[
- InlineKeyboardButton("Latvian",callback_data = "lv")
+ InlineKeyboardButton("Latvian",callback_data = "lang lv")
  ],[
- InlineKeyboardButton('Lithuanian',callback_data ="lt")
+ InlineKeyboardButton('Lithuanian',callback_data ="lang lt")
  ],[
- InlineKeyboardButton("Luxembourgish",callback_data = "lb")
+ InlineKeyboardButton("Luxembourgish",callback_data = "lang lb")
  ],[
- InlineKeyboardButton("Macedonian",callback_data = "mk")
+ InlineKeyboardButton("Macedonian",callback_data = "lang mk")
  ],[
- InlineKeyboardButton("Malagasy",callback_data ="mg")
+ InlineKeyboardButton("Malagasy",callback_data ="lang mg")
  ],[
- InlineKeyboardButton("Malay",callback_data ="ms")
+ InlineKeyboardButton("Malay",callback_data ="lang ms")
  ],[
- InlineKeyboardButton("Malayalam",callback_data = "ml")
+ InlineKeyboardButton("Malayalam",callback_data = "lang ml")
  ],[
- InlineKeyboardButton("Maltese",callback_data = "mt")
+ InlineKeyboardButton("Maltese",callback_data = "lang mt")
  ],[
- InlineKeyboardButton("Maori",callback_data = "mi")
+ InlineKeyboardButton("Maori",callback_data = "lang mi")
  ],[
- InlineKeyboardButton("Marathi",callback_data = "mr")
+ InlineKeyboardButton("Marathi",callback_data = "lang mr")
  ],[
- InlineKeyboardButton("Mongolian",callback_data = "mn")
+ InlineKeyboardButton("Mongolian",callback_data = "lang mn")
  ],[
- InlineKeyboardButton("Myanmar (Burmese)",callback_data = "my")
+ InlineKeyboardButton("Myanmar (Burmese)",callback_data = "lang my")
  ],[
- InlineKeyboardButton("Nepali",callback_data ="ne")
+ InlineKeyboardButton("Nepali",callback_data ="lang ne")
  ],[
- InlineKeyboardButton("Norwegian",callback_data = "no")
+ InlineKeyboardButton("Norwegian",callback_data = "lang no")
  ],[
- InlineKeyboardButton("Nyanja (Chichewa)",callback_data = "ny")
+ InlineKeyboardButton("Nyanja (Chichewa)",callback_data = "lang ny")
  ],[
- InlineKeyboardButton("Odia",callback_data = "or")
+ InlineKeyboardButton("Odia",callback_data = "lang or")
  ],[
- InlineKeyboardButton("Pashto",callback_data = "ps")
+ InlineKeyboardButton("Pashto",callback_data = "lang ps")
  ],[
- InlineKeyboardButton("Persian",callback_data = "fa")
+ InlineKeyboardButton("Persian",callback_data = "lang fa")
  ],[
- InlineKeyboardButton("Polish",callback_data = "pl")
+ InlineKeyboardButton("Polish",callback_data = "lang pl")
  ],[
- InlineKeyboardButton("Portuguese",callback_data = "pt")
+ InlineKeyboardButton("Portuguese",callback_data = "lang pt")
  ],[
- InlineKeyboardButton("Punjabi",callback_data = "pa")
+ InlineKeyboardButton("Punjabi",callback_data = "lang pa")
  ],[
- InlineKeyboardButton("Romanian",callback_data = "ro")
+ InlineKeyboardButton("Romanian",callback_data = "lang ro")
  ],[
- InlineKeyboardButton("Russian",callback_data = "ru")
+ InlineKeyboardButton("Russian",callback_data = "lang ru")
  ],[
- InlineKeyboardButton("Samoan",callback_data= "sm")
+ InlineKeyboardButton("Samoan",callback_data= "lang sm")
  ],[
- InlineKeyboardButton("Scots Gaelic",callback_data = "gd")
+ InlineKeyboardButton("Scots Gaelic",callback_data = "lang gd")
  ],[
- InlineKeyboardButton("Serbian",callback_data = "sr")
+ InlineKeyboardButton("Serbian",callback_data = "lang sr")
  ],[
- InlineKeyboardButton("Sesotho",callback_data = "st")
+ InlineKeyboardButton("Sesotho",callback_data = "lang st")
  ],[
- InlineKeyboardButton("Shona",callback_data ="sn")
+ InlineKeyboardButton("Shona",callback_data ="lang sn")
  ],[
- InlineKeyboardButton("Sindhi",callback_data ="sd")
+ InlineKeyboardButton("Sindhi",callback_data ="lang sd")
  ],[
- InlineKeyboardButton("Sinhala (Sinhalese)",callback_data = "si")
+ InlineKeyboardButton("Sinhala (Sinhalese)",callback_data = "lang si")
  ],[
- InlineKeyboardButton("Slovak",callback_data = "sk")
+ InlineKeyboardButton("Slovak",callback_data = "lang sk")
  ],[
- InlineKeyboardButton("Slovenian",callback_data = "sl")
+ InlineKeyboardButton("Slovenian",callback_data = "lang sl")
  ],[
- InlineKeyboardButton("Somali",callback_data = "so")
+ InlineKeyboardButton("Somali",callback_data = "lang so")
  ],[
- InlineKeyboardButton("Spanish",callback_data = "es")
+ InlineKeyboardButton("Spanish",callback_data = "lang es")
  ],[
- InlineKeyboardButton("Sundanese",callback_data ="su")
+ InlineKeyboardButton("Sundanese",callback_data ="lang su")
  ],[
- InlineKeyboardButton("Swahili",callback_data ="sw")
+ InlineKeyboardButton("Swahili",callback_data ="lang sw")
  ],[
- InlineKeyboardButton("Swedish",callback_data = "sv")
+ InlineKeyboardButton("Swedish",callback_data = "lang sv")
  ],[
- InlineKeyboardButton("Tagalog (Filipino)",callback_data ='tl')
+ InlineKeyboardButton("Tagalog (Filipino)",callback_data ='lang tl')
  ],[
- InlineKeyboardButton("Tajik",callback_data = "tg")
+ InlineKeyboardButton("Tajik",callback_data = "lang tg")
  ],[
- InlineKeyboardButton("Tamil",callback_data = "ta")
+ InlineKeyboardButton("Tamil",callback_data = "lang ta")
  ],[
- InlineKeyboardButton("Tatar",callback_data = "tt")
+ InlineKeyboardButton("Tatar",callback_data = "lang tt")
  ],[
- InlineKeyboardButton("Telugu",callback_data = "te")
+ InlineKeyboardButton("Telugu",callback_data = "lang te")
  ],[
- InlineKeyboardButton("Thai",callback_data = "th"),
+ InlineKeyboardButton("Thai",callback_data = "lang th"),
  ],[
- InlineKeyboardButton("Turkish",callback_data = "tr")
+ InlineKeyboardButton("Turkish",callback_data = "lang tr")
  ],[
- InlineKeyboardButton("Turkmen",callback_data ="tk")    
+ InlineKeyboardButton("Turkmen",callback_data ="lang tk")    
  ],[
- InlineKeyboardButton("Ukrainian",callback_data = "uk")
+ InlineKeyboardButton("Ukrainian",callback_data = "lang uk")
  ],[
- InlineKeyboardButton("Urdu",callback_data = "ur")
+ InlineKeyboardButton("Urdu",callback_data = "lang ur")
  ],[    
- InlineKeyboardButton("Uyghur",callback_data ="ug")
+ InlineKeyboardButton("Uyghur",callback_data ="lang ug")
  ],[     
- InlineKeyboardButton("Uzbek",callback_data = "uz")
+ InlineKeyboardButton("Uzbek",callback_data = "lang uz")
  ],[  
- InlineKeyboardButton("Vietnamese",callback_data ="vi")
+ InlineKeyboardButton("Vietnamese",callback_data ="lang vi")
  ],[   
- InlineKeyboardButton("Welsh",callback_data = "cy")
+ InlineKeyboardButton("Welsh",callback_data = "lang cy")
  ],[    
- InlineKeyboardButton("Xhosa",callback_data = "xh")
+ InlineKeyboardButton("Xhosa",callback_data = "lang xh")
  ],[   
- InlineKeyboardButton("Yiddish",callback_data = "yi")
+ InlineKeyboardButton("Yiddish",callback_data = "lang yi")
  ],[     
- InlineKeyboardButton("Yoruba",callback_data = "yo")
+ InlineKeyboardButton("Yoruba",callback_data = "lang yo")
  ]]
  )
