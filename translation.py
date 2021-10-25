096d69315b7b48ecd5d3628c8b36643fb3d72a9a
Muhammed <74817426+PR0FESS0R-99@users.noreply.github.com>
2021-10-25 18:59:46 +0530

    ✅️

--- translation.py
+++ translation.py
@@ -4,5 +4,8 @@ class Translation(object):
 
     TRANSLATED_MSG = """Choose The language From here That I Want to Translate.👇"""
 
-    ABOUT_MSG = """🤖 Bot : Google Translator\n\n👨‍🎓 Credit : @PR0FESS0R_99\n\n🎙️ Language : Python3\n\n📚 Lybrary : Pyrogram v1.2.9\n\n🛑 Server : Heroku\n\n📱 Build :V0.3"""
+    ABOUT_MSG = """🤖 Bot : Google Translator\n\n👨‍🎓 Credit : @PR0FESS0R_99\n\n🎙️ Language : Python3\n\n📚 Lybrary : Pyrogram v1.2.9\n\n🛑 Server : Heroku\n\n📱 Build :V0.4"""
+
+    CREDITS = """Credits Here"""
+
 
