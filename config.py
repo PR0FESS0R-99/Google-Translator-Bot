a5f6bb560f6d52ea727cd8c76d69a13724486e1d
Muhammed <74817426+PR0FESS0R-99@users.noreply.github.com>
2021-10-25 18:58:18 +0530

    ✅️

--- config.py
+++ config.py
@@ -1,12 +1,13 @@
-import os
-
 class Config(object):
      
     BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
 
-
     API_ID = int(os.environ.get("API_ID", 12345))
-
 
     API_HASH = os.environ.get("API_HASH", "")
 
+    DATABASE = os.environ.get("DATABASE", "")
+
+    DEV_NAME = os.environ.get("DEV_NAME", "Muhammed")
+
+    DEV_ID = set(int(x) for x in os.environ.get("DEV_ID", "1855070892").split())
